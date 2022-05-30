ad1efc577024570321f79459224a719136129fe8
NasrinAkter22 <258nasrinakter@gmail.com>
2022-05-30 17:39:23 +0600

    top car server intialies

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://{process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gsnphey.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Top car Parts')
+})
+
+app.listen(port, () => {
+    console.log(`Top car listening on port ${port}`)
+})
